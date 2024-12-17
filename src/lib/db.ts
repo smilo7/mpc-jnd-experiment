f135a3ac8606002102bac9a8f9f32e038a531ef7
smilo7 <smilo@protonmail.ch>
2024-12-17 19:08:51 +0100

    sort out .env loading in db util

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const MONGODB_URI = import.meta.env.MONGODB_URI;
+dotenv.config();
+
+const MONGODB_URI = process.env.MONGODB_URI || '';
 
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable');
